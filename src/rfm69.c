51c6bb1e0492f09e40d960f23c6635058ea0c4d8
matthieu <matthieu.deroo@etu.univ-smb.fr>
2025-12-04 21:33:37 +0100

    Biblioteque en cours de construction du skelet

--- src/rfm69.c
+++ src/rfm69.c
@@ -4,5 +4,9 @@ void RFM69_Init() {
     /*Reset le module RFM69*/
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
     HAL_Delay(1);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+    HAL_Delay(5);
+    /*module RFM69 reinitialiser*/
+
+    
 }
